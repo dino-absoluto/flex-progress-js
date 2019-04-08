3e6a52195123fb480d09c5bc972a795f8fce5e83
Dino <dinoabsoluto+dev@gmail.com>
2019-04-08 22:52:28 +0700

    Colorize output

--- src/test-run.ts
+++ src/test-run.ts
@@ -21,30 +21,53 @@
 /* imports */
 import delay from 'lodash-es/delay'
 import round from 'lodash-es/round'
+import flip from 'lodash-es/flip'
+import overArgs from 'lodash-es/overArgs'
+import chalk from 'chalk'
 import * as FlexBar from '.'
 /* exports */
 
 const out = new FlexBar.Output()
-out.add(new FlexBar.Spinner())
-out.add(new FlexBar.Spinner())
+out.add(new FlexBar.Space({ width: 2 }))
+out.add(new FlexBar.Spinner({ postProcess: chalk.yellow }))
+out.add(new FlexBar.Spinner({ postProcess: chalk.red }))
+out.add(new FlexBar.Spinner({ postProcess: chalk.cyan }))
 out.add(new FlexBar.Space())
-out.add(new FlexBar.Text({ text: 'Hello World!' }))
+const msg = new FlexBar.Text({ text: 'Hello World!', postProcess: chalk.green })
+out.add(msg)
 out.add(new FlexBar.Space())
-out.add(new FlexBar.Spinner())
-out.add(new FlexBar.Spinner())
-out.add(new FlexBar.Space())
-const bar = new FlexBar.Bar({ flex: 1 })
+out.add(new FlexBar.Spinner({ postProcess: chalk.cyan }))
+out.add(new FlexBar.Spinner({ postProcess: chalk.red }))
+out.add(new FlexBar.Spinner({ postProcess: chalk.yellow }))
+out.add(new FlexBar.Space({ width: 4 }))
+const bar = new FlexBar.Bar({ flex: 1, postProcess:
+  overArgs((...i: string[]) => i.join(''), chalk.green, chalk.yellow, chalk.gray)
+})
+const bar2 = new FlexBar.Bar({ flex: 1, postProcess:
+  overArgs(flip((...i: string[]) => i.join('')), chalk.green, chalk.yellow, chalk.gray)
+})
 out.add(bar)
+out.add(new FlexBar.Space())
+out.add(new FlexBar.Spinner({ postProcess: chalk.magenta }))
+out.add(new FlexBar.Space())
+out.add(bar2)
 
 let count = 0
 const int = setInterval(async () => {
   count++
+  msg.text = 'Hello World! ' + count
   bar.ratio = (count % 100) / 100
+  bar2.ratio = (count * 2 / 3 % 100) / 100
 }, 20)
 
 delay(() => {
   out.clear()
   const elapsed = out.elapsed
-  console.log('FPS', round(out.count * 1000 / elapsed, 1), 'Elapsed', elapsed)
+  console.log(
+    chalk.white('FPS'),
+    chalk.green(round(out.count * 1000 / elapsed, 1).toString()),
+    chalk.gray('|'),
+    chalk.white('Elapsed'),
+    chalk.green(elapsed.toString()))
   clearInterval(int)
 }, 5000)
