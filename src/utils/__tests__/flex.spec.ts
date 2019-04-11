1b40de8066bd4ee8a1e0277ee616604c8476068f
Dino <dinoabsoluto+dev@gmail.com>
2019-04-12 04:19:56 +0700

    Add flex.ts

---
+++ src/utils/__tests__/flex.spec.ts
@@ -0,0 +1,49 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * flex-progress-js - Progress indicator for Node.js
+ * Copyright (C) 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+/* imports */
+import { flex } from '../flex'
+
+/* code */
+describe('flex', () => {
+  test('grow', async () => {
+    const results = flex([
+      {
+        flexGrow: 0,
+        flexShrink: 0,
+        calculateWidth () { return 3 }
+      }, {
+        flexGrow: 1,
+        flexShrink: 0,
+        calculateWidth () { return 5 }
+      }, {
+        flexGrow: 1,
+        flexShrink: 0,
+        calculateWidth () { return 3 }
+      }, {
+        flexGrow: 2,
+        flexShrink: 0,
+        calculateWidth () { return 3 }
+      }
+    ], 20)
+    expect(results.reduce((acc, item) => acc + item.width, 0)).toBe(20)
+    console.log(results)
+  })
+})
---
+++ src/utils/flex.ts
@@ -0,0 +1,106 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * flex-progress-js - Progress indicator for Node.js
+ * Copyright (C) 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+/* imports */
+import clamp from 'lodash-es/clamp'
+
+/* code */
+// █████▒░░░░░░░░░
+// ██████▓░░░░░░░░
+// █████████████▓░
+// █▓▒░▒▓█
+
+interface FlexItem {
+  maxWidth?: number
+  minWidth?: number
+  flexGrow: number
+  flexShrink: number
+  calculateWidth (): number
+}
+
+interface FlexState {
+  width: number
+  growRoom: number
+  shrinkRoom: number
+  grow: number
+  shrink: number
+  item: FlexItem
+}
+
+const grow = (states: FlexState[], deltaW: number, flexSum: number) => {
+  const perFlex = deltaW / flexSum
+  for (const state of states) {
+    if (state.grow === 0) {
+      continue
+    }
+    const adjust = clamp(perFlex * state.grow, 0, Math.min(deltaW, state.growRoom))
+    deltaW -= adjust
+    state.width += adjust
+    if (deltaW === 0) {
+      break
+    }
+  }
+}
+
+const shrink = (states: FlexState[], deltaW: number, flexSum: number) => {
+  const perFlex = deltaW / flexSum
+  for (const state of states) {
+    if (state.shrink === 0) {
+      continue
+    }
+    const adjust = clamp(perFlex * state.shrink, 0, Math.min(deltaW, state.shrinkRoom))
+    deltaW -= adjust
+    state.width -= adjust
+    if (deltaW === 0) {
+      break
+    }
+  }
+}
+
+export const flex = (children: FlexItem[], maxWidth: number) => {
+  let states: FlexState[]
+  let growSum = 0
+  let shrinkSum = 0
+  let widthSum = 0
+  states = children.map((item) => {
+    const width = item.calculateWidth()
+    const growRoom = Math.min(item.maxWidth || maxWidth, maxWidth) - width
+    const shrinkRoom = width - Math.max(item.minWidth || 0, 0)
+    const grow = item.flexGrow * growRoom
+    const shrink = item.flexShrink * growRoom
+    growSum += grow
+    shrinkSum += shrink
+    widthSum += width
+    return {
+      width,
+      growRoom,
+      shrinkRoom,
+      grow,
+      shrink,
+      item
+    }
+  })
+  if (widthSum < maxWidth) {
+    grow(states, maxWidth - widthSum, growSum)
+  } else if (widthSum > maxWidth) {
+    shrink(states, widthSum - maxWidth, shrinkSum)
+  }
+  return states
+}
