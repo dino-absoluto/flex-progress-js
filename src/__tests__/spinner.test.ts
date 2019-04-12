aa419d3014fb613cf81e93e5bde00e0ddcbfd065
Dino <dinoabsoluto+dev@gmail.com>
2019-04-12 07:27:19 +0700

    Add spinner.ts

---
+++ src/__tests__/spinner.test.ts
@@ -0,0 +1,39 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * flex-progress-js - Progress indicator for Node.js
+ * Copyright (C) 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+/* imports */
+import { Spinner, themeDefault } from '../spinner'
+
+/* code */
+// █████▒░░░░░░░░░
+// ██████▓░░░░░░░░
+// █████████████▓░
+// █▓▒░▒▓█
+describe('Spinner', () => {
+  test('render()', async () => {
+    const spin = new Spinner()
+    for (let i = 0; i < 5; ++i) {
+      for (const frame of themeDefault.frames) {
+        expect(spin.render()).toBe(frame)
+        spin.tick(themeDefault.interval)
+      }
+    }
+  })
+})
---
+++ src/spinner.ts
@@ -0,0 +1,165 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * flex-progress-js - Progress indicator for Node.js
+ * Copyright (C) 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+/* imports */
+import { Base, BaseOptions, BaseData } from './base'
+import { ParentElement, SYNCING_INTERVAL } from './shared'
+import stringWidth from './optional/string-width'
+// import once from 'lodash-es/once'
+
+/* code */
+// █████▒░░░░░░░░░
+// ██████▓░░░░░░░░
+// █████████████▓░
+// █▓▒░▒▓█
+
+/** Describe Spinner theme */
+interface SpinnerTheme {
+  frames: string[]
+  interval: number
+  width?: number
+}
+
+interface SpinnerThemeSized extends SpinnerTheme {
+  width: number
+}
+
+export const themeDefault: SpinnerThemeSized = {
+  width: 1,
+  interval: 80,
+  frames:
+  [ '⠋'
+  , '⠙'
+  , '⠹'
+  , '⠸'
+  , '⠼'
+  , '⠴'
+  , '⠦'
+  , '⠧'
+  , '⠇'
+  , '⠏'
+  ]
+}
+
+Object.freeze(themeDefault)
+Object.freeze(themeDefault.frames)
+
+// const styleLine = {
+//   width: 1,
+//   interval: 120,
+//   frames:
+//   [ '~'
+//   , '\\'
+//   , '|'
+//   , '/'
+//   ]
+// }
+
+/** Describe options to class Spinner constructor() */
+interface SpinnerOptions extends BaseOptions {
+  theme?: SpinnerTheme
+}
+
+interface SpinnerData extends BaseData {
+  theme: SpinnerThemeSized
+  autoTicking: boolean
+  time: number
+  frame: number
+  frameOffset: number
+}
+
+/** Busy Spinner */
+export class Spinner<T extends SpinnerData> extends Base<T> {
+  constructor (options: SpinnerOptions = {}) {
+    super(options)
+    if (options.theme) {
+      this.theme = options.theme
+    }
+  }
+
+  get time () { return this.proxy.time || 0 }
+  set time (frame: number) {
+    this.proxy.time = frame
+  }
+
+  get frame () { return this.proxy.frame || 0 }
+  set frame (frame: number) {
+    this.proxy.frame = frame
+  }
+
+  get frameOffset () { return this.proxy.frameOffset || 0 }
+  set frameOffset (offset: number) {
+    this.proxy.frameOffset = offset
+  }
+
+  tick (interval?: number) {
+    const theme = this.theme as SpinnerThemeSized
+    const time = (this.time || 0) + (interval || SYNCING_INTERVAL)
+    this.time = time
+    this.frame =
+      Math.floor(Math.round(time / SYNCING_INTERVAL)
+        / Math.round((theme.interval / SYNCING_INTERVAL)))
+  }
+
+  get autoTicking () { return this.enabled && (this.proxy.autoTicking || true) }
+  set autoTicking (auto: boolean) {
+    this.proxy.autoTicking = auto
+    // if (auto) {
+    //   this.pStart()
+    // }
+  }
+
+  /** Style to display spinner as */
+  get theme () { return this.proxy.theme || themeDefault }
+  set theme (spinner: SpinnerTheme) {
+    if (!spinner.width) {
+      spinner.width = stringWidth(spinner.frames[0])
+    }
+    this.proxy.theme = spinner as SpinnerThemeSized
+    // this.pFrame = 0
+  }
+
+  private pHandleSync = (frame: number) => {
+    if (this.parent && this.autoTicking) {
+      const { frame: oldFrame, theme } = this
+      frame = Math.floor(frame / Math.round((theme.interval / SYNCING_INTERVAL)))
+      if (oldFrame !== frame) {
+        this.frame = frame
+      }
+      this.parent.nextFrame(this.pHandleSync)
+    }
+  }
+
+  protected handleCalculateWidth (): number {
+    return (this.theme as SpinnerThemeSized).width
+  }
+
+  protected handleRender (maxWidth?: number) {
+    const { frame, frameOffset, parent } = this
+    const theme = this.theme as SpinnerThemeSized
+    if (maxWidth != null && maxWidth < theme.width) {
+      return ''
+    }
+    if (parent) {
+      parent.nextFrame(this.pHandleSync)
+    }
+    return theme.frames[(frame + frameOffset) % theme.frames.length]
+  }
+}
