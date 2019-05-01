d19386d12c870c4407d199faad1b83f32b495af2
Dino <dinoabsoluto+dev@gmail.com>
2019-05-01 17:37:43 +0700

    Add demo-subgroup

---
+++ examples/demo-subgroup.js
@@ -0,0 +1,50 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * Copyright 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/* imports */
+const FlexProgress = require('..')
+// const FlexProgress = require('@dinoabsoluto/flex-progress')
+// ESModules or Typescript
+// import * as FlexProgress from '@dinoabsoluto/flex-progress'
+
+const out = new FlexProgress.Output()
+const bar = new FlexProgress.Bar({ width: 25 })
+const group = new FlexProgress.Group()
+
+group.append(
+  1, new FlexProgress.Spinner(),
+  1, 'Hello World!',
+  1, new FlexProgress.Spinner(),
+  1, '⸨', bar, '⸩'
+)
+out.append(group)
+
+let count = 0
+const loop = setInterval(() => {
+  count++
+  bar.ratio = (count % 39) / 38
+}, 80)
+
+/* End the loop */
+if (!(process.argv.indexOf('--loop') >= 0)) {
+  setTimeout(() => {
+    clearInterval(loop)
+    /* Clear elements from output */
+    out.clear()
+  }, 10000)
+}
